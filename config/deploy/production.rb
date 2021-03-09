3ad029254f624cae68e2dba50c4f8375a8e17f0d
nanakuma <nanakumananakuma13096@gmail.com>
2021-03-09 16:52:15 +0900

    edit productin.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '3.132.248.17 ', user: 'ec2-user', roles: %w{app db web}
+server '3.132.248.17', user: 'ec2-user', roles: %w{app db web}
